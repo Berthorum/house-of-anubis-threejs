fd653a9625c12cdc7def87e62360fec0a0675210
Berthorum <ravier.programa@hotmail.com>
2023-10-01 19:17:16 -0300

    Update dependencies

--- src/threejs/scene.js
+++ src/threejs/scene.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // import model from '../assets/mini_room_art.glb';
 
 function main() {
